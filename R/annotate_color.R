6680d9f61fe8997090cf51aa9cf23beb39792c85
Chris Peralta <chris99994@gmail.com>
2018-06-13 12:10:39 +0800

    Initial commit

---
+++ R/annotate_color.R
@@ -0,0 +1,50 @@
+## This function will allow you to assign color to each word in a plot annotation
+# Dependencies: purrr, ggplot2
+
+annotate_color <- function(geom = 'text', x, y, xmin = NULL, xmax = NULL,  
+                           ymin = NULL, ymax = NULL, xend = NULL, yend = NULL, ...,
+                           labels, colors, default_color = 'black'){
+  
+  
+  labels <- strsplit(labels, " ")[[1]] 
+  n <- length(labels)
+  
+  if (length(colors) < length(labels)){   # Assigns any empty values in 'colors' to the 'default_color' 
+    colors <- purrr::map_chr(seq_len(length(labels)), function(i){
+      if (is.na(colors[i]) | colors[i] == ''){
+        colors[i] <- default_color
+      } else {colors[i] <- colors [i]}}
+    )
+  }
+  
+  if (length(colors) > length(labels)){   # Shortens the length of 'colors' to match the length of 'labels'
+    colors = colors[1:length(labels)]
+    warning('The length of the colors arg is longer than the number of words in the labels arg. Extra colors will be               ignored.')
+  }
+  
+  # Formats the labels argument into usable arguments for each annotation function
+  labels <- purrr::map_chr(seq_len(n), function(i) {  
+    start0 <- labels[seq_along(labels) < i]    # Assigns the first part of the string 
+    mid0 <- labels[i]                          # Assigns a single word 
+    end0 <- labels[seq_along(labels) > i]      # Assigns the last part of the string
+    start <- paste0('phantom("', paste(start0, collapse = " "), ' ")') # Wraps phantom() around the first part 
+    end <- paste0('phantom("', paste(end0, collapse = " "), ' ")') # Wraps phantom() around the last part
+    if(length(start0) > 0 && length(end0) > 0) {  # Conditional statements for the formatting depending...
+      paste(start, paste0('"', paste(mid0, collapse = " "), '"'), end, sep = ' * ') # ... on the position of 'mid0'
+    } else if (length(end0) > 0) {
+      paste(paste0('"', paste(mid0, collapse = " "), '"'), end, sep = ' * ')
+    } else if (length(start0) > 0) {
+      paste(start, paste0('"', paste(mid0, collapse = " "), '"'), sep = ' * ')
+    } else {
+      stop("couldn't finish ...")
+    } # Anonymous function above created with the assistance of
+  })  # https://stackoverflow.com/users/3521006/docendo-discimus
+  
+  # Plugs all arguments into the annotate() function and stores them into a list
+  annofuncs <- list()
+  annofuncs <- purrr::map2(labels, colors, function(annolabel, annocolor){
+    annofuncs[seq_along(annolabel)] <- list(annotate(geom, x, y, xmin, xmax, ymin, ymax, xend, yend, ...,
+                                                     parse = T, label = annolabel, color = annocolor))
+  })
+  return(annofuncs) # Returns the list which can be added to a ggplot like any other layer
+}
---
+++ R/word_count.R
@@ -0,0 +1,42 @@
+#' Gives word frequencies from a character or factor column of a tibble
+#' 
+#' Takes a tibble and character column name as input, then returns the 
+#' frequencies of that column in a new tibble.  
+#' 
+#' @param .data A tibble.
+#' @param .char_col The name of a character or factor column in the tibble.
+#' 
+#' @return tibble with a column for word and a column for the frequency
+#' 
+#' @example 
+#' iris %>% 
+#'   as.tibble() %>% 
+#'   word_count(Species)
+#'   
+
+require(tidyverse)
+require(stringr)
+
+word_count <- function(.data, .char_col) {
+
+  if(!is.tibble(.data)) {warning(".data is not a tibble, errors may occur")}
+  
+  .char_col <- enquo(.char_col)
+  
+  .data %>% 
+    select(!!.char_col) %>% 
+    select_if(function(x) is.factor(x) || is.character(x)) %>% # Gives error when col type isn't chr or fct
+    mutate(word = str_remove_all(!!.char_col, '[[:punct:]]')) %>% # Removes punctuation
+    mutate(word = str_split(word, ' ')) %>% #Separate words into a vec of strings
+    unnest() %>%                           # Expands the list column into a char column
+    filter(word != "") %>%                 # Remove all empty strings
+    mutate(word = str_to_lower(word)) %>%  # Puts everything in lowercarse
+    group_by(word) %>%                     # Groups by word
+    summarize(freq = n()) %>%              # Calculates the number of each word
+    arrange(desc(freq))                    # Sorts the tibble by descending frequency
+}
+# Catches the error thrown by select_if and gives an error message
+word_count <- purrr::possibly(
+                        .f = word_count, 
+                        otherwise = 'char_col should be a character column (or factor)', 
+                        quiet = FALSE)
